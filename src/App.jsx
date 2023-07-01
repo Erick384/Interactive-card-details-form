3392427d7580264a88b15d3ce4c0c748512da86a
Erick384 <erickwachuma@gmail.com>
2023-07-01 23:50:38 +0300

    Uploading Commits

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,38 @@ function App() {
 
   return (
     <div className="body">
+      <div className="left">
+        <div className="jane-Card">
+          <div className="logo">
+            <svg
+              width="84"
+              height="47"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <ellipse
+                cx="23.478"
+                cy="23.5"
+                rx="23.478"
+                ry="23.5"
+                fill="#fff"
+              />
+              <path
+                d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z"
+                stroke="#fff"
+              />
+            </svg>
+          </div>
+          <div className="card-Number">
+            <h2>0000 0000 0000 0000</h2>
+          </div>
+          <div className="card-Btm">
+            <span>Jane Appleseed</span>
+            <span>00/00</span>
+          </div>
+        </div>
+        <div className="back-Page"></div>
+      </div>
       {/* Your existing code */}
       <div className="right">
         <form className="form" onSubmit={handleSubmit}>
