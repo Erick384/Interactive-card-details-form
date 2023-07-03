d267baf618e728e5ae4a5294ab891c73ccc3e247
Erick384 <erickwachuma@gmail.com>
2023-07-03 14:26:10 +0300

    Uploading Commits

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
             </svg>
           </div>
           <div className="card-Number">
-            <h2>0000 0000 0000 0000</h2>
+            <h1>0000  0000  0000  0000</h1>
           </div>
           <div className="card-Btm">
             <span>Jane Appleseed</span>
